349a5833a021f9a4f2f5bb785b02f63db8db8915
VasilichFadeev <keksonianin@gmail.com>
2025-03-22 04:11:30 +0700

    Delete src/main/java/module-info.java

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.laba_1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-    requires javafx.graphics;
-    opens org.example.laba_1 to javafx.fxml;
-    exports org.example.laba_1;
-}
