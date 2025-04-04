44aacc80bdcbbeea7e4c0ebce1413798837c9423
pandoras <pandoras@edu.21-school.ru>
2025-04-05 02:46:53 +0700

    Repushed module-info.java to resolve conflicts after refactoring

--- ЛР2/Files/src/main/java/module-info.java
+++ ЛР2/Files/src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module org.example.laba_1 {
+module org.example.laba_2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
     requires javafx.graphics;
     requires java.desktop;
-    opens org.example.laba_1 to javafx.fxml;
-    exports org.example.laba_1;
+    opens org.example.laba_2 to javafx.fxml;
+    exports org.example.laba_2;
 }
