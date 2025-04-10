1e7ba12c0bf6345e3d686c3458307b753d76d4ae
Vasiliy <keksonianin@gmail.com>
2025-04-10 14:34:39 +0700

    Uploaded PokemonInfo.java, made new class fields for working with android

---
+++ ПР2/app/src/main/java/com/example/practics_2/PokemonInfo.java
@@ -0,0 +1,15 @@
+package com.example.practics_2;
+
+public class PokemonInfo {
+    public String name;
+    public String url;
+    public int height;
+    public int weight;
+
+    public PokemonInfo(String name, String url, int height, int weight) {
+        this.name = name;
+        this.url = url;
+        this.height = height;
+        this.weight = weight;
+    }
+}
