bc53dda4b5df3cf5d2a8ba5030129fb2cf00476c
pandoras <pandoras@edu.21-school.ru>
2025-04-19 02:00:35 +0700

    Added variables of life, id generation

---
+++ ЛР3/Files/src/main/java/org/example/laba_3/GameObject.java
@@ -0,0 +1,99 @@
+package org.example.laba_3;
+
+import javafx.scene.image.ImageView;
+import java.util.Objects;
+
+public abstract class GameObject implements IBehaviour {
+    protected ImageView imageView;
+    protected double speedX;
+    protected double speedY;
+    protected long birthTime; // Время рождения объекта (наносекунды)
+    protected long lifetime;  // Время жизни объекта (наносекунды)
+    protected int id;         // Уникальный идентификатор объекта
+
+    public GameObject() {
+        this.id = generateUniqueId();
+    }
+
+    /**
+     * Генерирует уникальный идентификатор для объекта
+     * @return уникальный ID
+     */
+    private int generateUniqueId() {
+        int newId;
+        do {
+            newId = (int)(Math.random() * Integer.MAX_VALUE);
+        } while (!Habitat.getInstance().isIdUnique(newId));
+        return newId;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public long getBirthTime() {
+        return birthTime;
+    }
+
+    public long getLifetime() {
+        return lifetime;
+    }
+
+    public void setLifetime(long lifetime) {
+        this.lifetime = lifetime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Проверяет, истекло ли время жизни объекта
+     * @param currentTime текущее время симуляции в наносекундах
+     * @return true, если время жизни истекло
+     */
+    public boolean isExpired(long currentTime) {
+        return currentTime - birthTime >= lifetime;
+    }
+
+    @Override
+    public abstract void update(int maxWidth, int maxHeight);
+
+    /**
+     * Обрабатывает столкновение с границами области
+     * @param maxWidth максимальная ширина области
+     * @param maxHeight максимальная высота области
+     */
+    protected void checkBoundaryCollision(double maxWidth, double maxHeight) {
+        double x = imageView.getX();
+        double y = imageView.getY();
+        double width = imageView.getFitWidth();
+        double height = imageView.getFitHeight();
+
+        if (x <= 0 || x + width >= maxWidth) speedX = -speedX;
+        if (y <= 0 || y + height >= maxHeight) speedY = -speedY;
+
+        imageView.setX(Math.max(0, Math.min(x, maxWidth - width)));
+        imageView.setY(Math.max(0, Math.min(y, maxHeight - height)));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameObject that = (GameObject) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [id=" + id +
+                ", birthTime=" + birthTime +
+                ", lifetime=" + lifetime + "]";
+    }
+}
